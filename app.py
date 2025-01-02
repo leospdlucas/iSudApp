ed9d3fce12b8b1c659c2ab74943233b230b44766
leolucas <luc24014@byui.edu>
2025-01-02 02:57:22 -0300

    App.run

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -32,5 +34,6 @@ def log(month, week, day):
     print(f"Log: {month} {week} - Day {day} marked as Missão")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
